5e457c742f300327f3863237b6314f91742dc7a5
SebaKo23 <100848601+SebaKo23@users.noreply.github.com>
2023-10-21 09:29:32 +0200

    lab3.2

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import numpy as np
 
+
 # zad 1
 def rysuj_pasy_pionowe_szare(w, h, grub, zmiana_koloru):
     t = (h, w)
@@ -13,6 +14,7 @@ def rysuj_pasy_pionowe_szare(w, h, grub, zmiana_koloru):
                 tab[i, j] = (k + zmiana_koloru) % 256
     return Image.fromarray(tab)
 
+
 def rysuj_ramke_przemiennie_szare(w, h, grub, zmiana_koloru):
     t = (h, w)
     tab = np.zeros(t, dtype=np.uint8)
@@ -20,6 +22,7 @@ def rysuj_ramke_przemiennie_szare(w, h, grub, zmiana_koloru):
     for k in range(ile):
         tab[grub * k:h - grub * k, grub * k:w - grub * k] = (k + zmiana_koloru) % 256
     return Image.fromarray(tab)
+
 
 def negatyw_szare(obraz):
     tab = np.asarray(obraz)
@@ -30,8 +33,9 @@ def negatyw_szare(obraz):
             tab_neg[i, j] = 255 - tab[i, j]
     return Image.fromarray(tab_neg)
 
-ob1_1 = rysuj_pasy_pionowe_szare(480, 320, 10, 35)
-ob1_2 = rysuj_ramke_przemiennie_szare(480, 320, 10, 70)
+
+ob1_1 = rysuj_pasy_pionowe_szare(480, 320, 10, 60)
+ob1_2 = rysuj_ramke_przemiennie_szare(480, 320, 10, 60)
 ob1_1_neg = negatyw_szare(ob1_1)
 ob1_2_neg = negatyw_szare(ob1_2)
 ob1_1.save("obraz1_1.png")
@@ -42,6 +46,7 @@ ob1_1_neg.save("obraz1_1N.png")
 ob1_1_neg.save("obraz1_1N.jpg")
 ob1_2_neg.save("obraz1_2N.png")
 ob1_2_neg.save("obraz1_2N.jpg")
+
 
 # zad 2
 
@@ -54,11 +59,12 @@ def rysuj_pasy_pionowe_kolor(w, h, grub, kolor, zmiana_koloru):
         r = (kolor[0] + k * zmiana_koloru) % 256
         g = (kolor[1] + k * zmiana_koloru) % 256
         b = (kolor[2] + k * zmiana_koloru) % 256
-        for g in range(grub):
-            j = k * grub + g
+        for c in range(grub):
+            j = k * grub + c
             for i in range(h):
                 tab[i, j] = [r, g, b]
     return Image.fromarray(tab)
+
 
 def rysuj_ramke_przemiennie_kolor(w, h, grub, kolor, zmiana_koloru):
     t = (h, w, 3)
@@ -72,6 +78,7 @@ def rysuj_ramke_przemiennie_kolor(w, h, grub, kolor, zmiana_koloru):
         tab[grub * k:h - grub * k, grub * k:w - grub * k] = [r, g, b]
     return Image.fromarray(tab)
 
+
 def negatyw_kolor(obraz):
     tab = np.asarray(obraz)
     h, w, kolor = tab.shape
@@ -82,18 +89,22 @@ def negatyw_kolor(obraz):
                 tab_neg[i, j] = 255 - tab[i, j]
     return Image.fromarray(tab_neg)
 
-obraz2 = rysuj_ramke_przemiennie_kolor(480, 320, 10, [255, 255, 128], 32)
-obraz2N = negatyw_kolor(obraz2)
-obraz2.save("obraz2.png")
-obraz2.save("obraz2.jpg")
-obraz2N.save("obraz2N.png")
-obraz2N.save("obraz2N.jpg")
+
+obraz2ramka = rysuj_ramke_przemiennie_kolor(480, 320, 10, [64, 0, 128], 32)
+obraz2pasy = rysuj_pasy_pionowe_kolor(480, 320, 10, [64, 0, 128], 32)
+obraz2ramkaN = negatyw_kolor(obraz2ramka)
+obraz2pasyN = negatyw_kolor(obraz2pasy)
+obraz2pasy.save("obraz2.png")
+obraz2ramka.save("obraz2.jpg")
+obraz2pasyN.save("obraz2N.png")
+obraz2ramkaN.save("obraz2N.jpg")
+
 
 # zad 3
 def koloruj_inicjaly(obraz, kolor, zmiana_koloru):
     t_obraz = np.asarray(obraz)
     h, w = t_obraz.shape
-    t =(h, w, 3)
+    t = (h, w, 3)
     tab = np.ones(t, dtype=np.uint8)
     tab[:] = kolor
     for i in range(h):
@@ -101,12 +112,14 @@ def koloruj_inicjaly(obraz, kolor, zmiana_koloru):
         g = (kolor[1] + i * zmiana_koloru) % 256
         b = (kolor[2] + i * zmiana_koloru) % 256
         for j in range(w):
-            if t_obraz[i, j] == False:
+            if not t_obraz[i, j]:
                 tab[i, j] = [r, g, b]
             else:
                 tab[i, j] = [255, 255, 255]
     return Image.fromarray(tab)
 
+
 inicjaly = Image.open("inicjaly.bmp")
-kolorowe_inicjaly = koloruj_inicjaly(inicjaly, [128, 0, 128], 64)
-kolorowe_inicjaly.show()
+obraz3 = koloruj_inicjaly(inicjaly, [128, 0, 128], 64)
+obraz3.save("obraz3.png")
+obraz3.save("obraz3.jpg")
